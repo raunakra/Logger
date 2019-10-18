fb6e9c8bcc0f771bebb2e2b00de3b79abe0761d4
Raunak <rsingh@tavisca.in>
2019-10-18 07:19:12 +0530

    Add project files.

---
+++ MyDemo/Logger/ApiLog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+namespace MyDemo.Logger
+{
+    public class ApiLog : ITxsLogger
+    {
+        public void doLog()
+        {
+         Log.Information   
+        }
+    }
+}
---
+++ MyDemo/Logger/EventLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDemo.Logger
+{
+    public class EventLogger : LogBase
+    {
+        public override void Log(string message)
+        {
+            EventLog eventLog = new EventLog(“”);
+            eventLog.Source = "IDGEventLog";
+            eventLog.WriteEntry(message);
+        }
+    }
+}
